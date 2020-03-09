5d9f07beffd7afdbf7bf1efbed6f421b46bea158
Chantal Neo <Chantal@hotmail.sg>
2020-03-09 22:10:40 +0800

    Add Content to the Recipes Components, Output a List of Recipes and Display Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Soya Beancurd', 
+      'A simple, custard pudding that is light and cooling!',
+      'https://cdn.pixabay.com/photo/2016/07/28/00/13/japan-1546767_960_720.jpg'),
+    new Recipe(
+      'Fudge Brownies', 
+      'A chocolaty baked treat! Yum!',
+      'https://storage.needpix.com/rsynced_images/fudge-brownies-1235430_1280.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
