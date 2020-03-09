aad41126237679ac28d64a92aa90c49b7c20fcf2
Chantal Neo <Chantal@hotmail.sg>
2020-03-09 18:15:35 +0800

    Creating the Components

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-shopping-list',
+    template: './shopping-list.component.html',
+    styles: ['./shopping-list.component.css']
+})
+
+export class ShoppingListComponent {
+
+}
