b72b65bc45068a59f8265624800c3165200dee98
Chantal Neo <Chantal@hotmail.sg>
2020-03-11 22:36:46 +0800

    Passing Ingredients from Recipes to the Shopping List (via a Service)

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -19,4 +19,12 @@ export class ShoppingListService {
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
     }
+
+    addIngredients(ingredients: Ingredient[]) {
+        // for (let ingredient of ingredients) { // Viable, but bad because there would be a lot of unnecessary event emissions
+        //     this.addIngredient(ingredient);
+        // }
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice()); // Use spread operator, simple the three dots
+    }
 }
