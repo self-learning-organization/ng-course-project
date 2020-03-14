738d42ce65c905db258f392a749cdfa217880bfb
Chantal Neo <Chantal@hotmail.sg>
2020-03-14 23:47:42 +0800

    Redirecting the User (after Deleting a Recipe)

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -40,6 +40,6 @@ export class RecipeDetailComponent implements OnInit {
 
   onDeleteRecipe() {
     this.recipeService.deleteRecipe(this.id);
-    // this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['/recipes']);
   }
 }
