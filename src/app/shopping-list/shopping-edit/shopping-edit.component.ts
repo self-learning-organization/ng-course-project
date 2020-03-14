21859fb0df46e6c595813eb3e484bde56759bed1
Chantal Neo <Chantal@hotmail.sg>
2020-03-14 21:25:19 +0800

    Allowing the Deletion of Shopping List Items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -52,6 +52,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         this.subscription.unsubscribe();
     }
 
+    onDelete() {
+        this.shoppingListService.deleteIngredient(this.editedItemIndex);
+        this.onClear();
+    }
+
     onClear() {
         this.editMode = false;
         this.shoppingListForm.reset();
