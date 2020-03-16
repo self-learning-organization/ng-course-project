a2d3f9ee2af350936cbe1a7e22d73d3ad3912e6c
Chantal Neo <Chantal@hotmail.sg>
2020-03-16 18:23:09 +0800

    Transforming Response Data

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -8,34 +8,35 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 @Injectable()
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
+    private recipes: Recipe[] = [];
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Soya Beancurd',
-            'A simple, custard pudding that is light and cooling!',
-            'https://cdn.pixabay.com/photo/2016/07/28/00/13/japan-1546767_960_720.jpg',
-            [
-                new Ingredient('Polleney Soybean Powder', 60, 'grams'), 
-                new Ingredient('Unisoy Instant Organic Soya Milk Powder', 30, 'grams'),
-                new Ingredient('Nestlé Coffee-Mate', 30, 'grams'),
-                new Ingredient('RedMan Instant Jelly Powder', 13, 'grams'),
-                new Ingredient('Vanilla Powder', 1, 'tablespoon')
-            ]),
-        new Recipe(
-            'Fudge Brownies',
-            'A chocolaty baked treat! Yum!',
-            'https://storage.needpix.com/rsynced_images/fudge-brownies-1235430_1280.jpg',
-            [
-                new Ingredient('Coconut Flour', 0.25, 'cup'), 
-                new Ingredient('Unsweetened Cocoa Powder', 0.75, 'cup'),
-                new Ingredient('Salt', 0.5, 'teaspoon'),
-                new Ingredient('Ground Psyllium Husk', 0.5, 'tablespoon'),
-                new Ingredient('Large Eggs', 2, 'unit'),
-                new Ingredient('Vanilla Extract', 2, 'teaspoons'),
-                new Ingredient('Coconut Oil', 0.33, 'cup'),
-                new Ingredient('Honey', 0.25, 'cup')
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Soya Beancurd',
+    //         'A simple, custard pudding that is light and cooling!',
+    //         'https://cdn.pixabay.com/photo/2016/07/28/00/13/japan-1546767_960_720.jpg',
+    //         [
+    //             new Ingredient('Polleney Soybean Powder', 60, 'grams'), 
+    //             new Ingredient('Unisoy Instant Organic Soya Milk Powder', 30, 'grams'),
+    //             new Ingredient('Nestlé Coffee-Mate', 30, 'grams'),
+    //             new Ingredient('RedMan Instant Jelly Powder', 13, 'grams'),
+    //             new Ingredient('Vanilla Powder', 1, 'tablespoon')
+    //         ]),
+    //     new Recipe(
+    //         'Fudge Brownies',
+    //         'A chocolaty baked treat! Yum!',
+    //         'https://storage.needpix.com/rsynced_images/fudge-brownies-1235430_1280.jpg',
+    //         [
+    //             new Ingredient('Coconut Flour', 0.25, 'cup'), 
+    //             new Ingredient('Unsweetened Cocoa Powder', 0.75, 'cup'),
+    //             new Ingredient('Salt', 0.5, 'teaspoon'),
+    //             new Ingredient('Ground Psyllium Husk', 0.5, 'tablespoon'),
+    //             new Ingredient('Large Eggs', 2, 'unit'),
+    //             new Ingredient('Vanilla Extract', 2, 'teaspoons'),
+    //             new Ingredient('Coconut Oil', 0.33, 'cup'),
+    //             new Ingredient('Honey', 0.25, 'cup')
+    //         ])
+    // ];
 
     constructor(private shoppingListService: ShoppingListService) {
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipes.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -18,6 +19,11 @@ export class DataStorageService {
 
     fetchRecipes() {
         this.http.get<Recipe[]>('https://angular-course-recipe-bo-30749.firebaseio.com/recipes.json')
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+            });
+        }))
         .subscribe(recipes => {
             this.recipeService.setRecipes(recipes);
         });
