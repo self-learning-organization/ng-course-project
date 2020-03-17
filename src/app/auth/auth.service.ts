36ce30c694fe68e73da6d76102e88ed2324a6d51
Chantal Neo <Chantal@hotmail.sg>
2020-03-17 17:14:19 +0800

    Adding Auto-Login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -40,6 +40,29 @@ export class AuthService  {
         );
     }
 
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string; 
+            _token: string; 
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new User(
+            userData.email, 
+            userData.id, 
+            userData._token, 
+            new Date(userData._tokenExpirationDate)
+        );
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     login(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC7ZUYZ_7lFa4kXm_hYFN2WxoiX4HSp-TM', {
             email: email,
@@ -76,6 +99,7 @@ export class AuthService  {
             expirationDate
         );
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
