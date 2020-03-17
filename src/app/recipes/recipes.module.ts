680f4256923b045227e6a26362407a53e54c2c2f
Chantal Neo <Chantal@hotmail.sg>
2020-03-17 23:22:57 +0800

    Component Declarations

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -26,14 +26,6 @@ import { RecipesRoutingModule } from './recipes-routing.module';
         CommonModule,
         ReactiveFormsModule,
         RecipesRoutingModule
-    ],
-    exports: [
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeItemComponent,
-        RecipeDetailComponent,
-        RecipeStartComponent,
-        RecipeEditComponent
     ]
 })
 export class RecipesModule {}
