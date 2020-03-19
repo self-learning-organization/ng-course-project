4b6269c26c6901e7e0c84da20980e2718ce2dd0d
Chantal Neo <Chantal@hotmail.sg>
2020-03-19 17:48:51 +0800

    One Root State

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -5,7 +5,7 @@ import { Store } from '@ngrx/store';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
-import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
+import * as fromApp from '../store/app.reducer';
 
 @Injectable()
 export class RecipeService {
@@ -41,7 +41,7 @@ export class RecipeService {
     // ];
 
     constructor(
-        private store: Store<fromShoppingList.AppState>) {
+        private store: Store<fromApp.AppState>) {
     }
 
     setRecipes(recipes: Recipe[]) {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,8 +4,8 @@ import { Store } from '@ngrx/store';
 
 import { Ingredient } from '../shared/ingredient.model'
 import { LoggingService } from '../logging.service';
-import * as fromShoppingList from './store/shopping-list.reducer'; // "from..."'s a convention for describing an import to your reducer and/or your state for a certain part of your application
 import * as ShoppingListActions from './store/shopping-list.actions';
+import * as fromApp from '../store/app.reducer';
 
 @Component({
     selector: 'app-shopping-list',
@@ -19,7 +19,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
     constructor(
         private loggingService: LoggingService,
-        private store: Store<fromShoppingList.AppState>) {
+        private store: Store<fromApp.AppState>) {
     }
 
     ngOnInit() {
