3800504ed3841bc0f3b0f8df40f02a1ee9030801
Chantal Neo <Chantal@hotmail.sg>
2020-03-19 13:37:24 +0800

    Dispatching Actions

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import { Ingredient } from 'src/app/shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+import * as ShoppingListActions from '../store/shopping-list.actions';
 
 @Component({
     selector: 'app-shopping-edit',
@@ -17,7 +20,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     editedItemIndex: number;
     editedItem: Ingredient;
 
-    constructor(private shoppingListService: ShoppingListService) {
+    constructor(private shoppingListService: ShoppingListService, private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) {
     }
 
     ngOnInit() {
@@ -42,7 +45,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         if (this.editMode) {
             this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
         } else {
-            this.shoppingListService.addIngredient(newIngredient);
+            // this.shoppingListService.addIngredient(newIngredient);
+            this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
         }
         this.editMode = false;
         form.reset();
--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -6,5 +6,6 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
-    payload: Ingredient; // Payload isn't a name I have to use. The Action interface only forces to add a "type" property
+    // payload: Ingredient; // Payload isn't a name I have to use. The Action interface only forces to add a "type" property
+    constructor(public payload: Ingredient) {}
 }
